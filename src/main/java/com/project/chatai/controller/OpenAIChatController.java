3e5221404e6510d49fd05beed96237d01f4f46b1
tahatopal <taha.topal.7@gmail.com>
2026-04-26 21:42:13 +0300

    Adding template

--- src/main/java/com/project/chatai/controller/OpenAIChatController.java
+++ src/main/java/com/project/chatai/controller/OpenAIChatController.java
@@ -14,7 +14,7 @@ public class OpenAIChatController {
 
   private final ChatClient chatClient;
 
-  private final String systemPrompt = "You are a helpful assistant to summarize any given content."
+  private final String SYSTEM_PROMPT = "You are a helpful assistant to summarize any given content."
       + " Make sure the summary is concise, informative, etc."
       + " Do not answer anything other than the summarization."
       + " If the question is not about summarization,"
@@ -27,10 +27,33 @@ public class OpenAIChatController {
   @PostMapping("/summarize")
   public ChatClientResponse summarize(@RequestBody String message) {
     return chatClient.prompt()
-        .system(systemPrompt) // Setting the guardrail
+        .system(SYSTEM_PROMPT) // Setting the guardrail
         .user(message)
         .call()
         .chatClientResponse();
+  }
+
+  @PostMapping("/template")
+  public String summarizeSpecifically(@RequestBody String introduction) {
+    return chatClient.prompt()
+        .system(SYSTEM_PROMPT)
+        .user(u -> u.text("I want to summarize the following business introduction: \"{introduction}\""
+            + "Use following example to construct your response:\n"
+            + "Company introduction:\n"
+            + "\"Turkey’s largest technology consultancy, pioneering product development and technology consulting"
+            + " for an AI-native future. With more than 20 years of experience,"
+            + " we empower leading companies in banking and finance, insurance ecosystem.\"\n"
+            + "Example Output:\n"
+            + "\" Features:\n"
+            + "* Large technology company\n"
+            + "* +20 years of experiance\n"
+            + "  Domains:\n"
+            + "* Banking\n"
+            + "* Finance\n"
+            + "* Insurance\" ")
+            .param("introduction", introduction))
+        .call()
+        .content();
   }
 
 }
